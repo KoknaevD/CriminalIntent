b34fd8d320c3d1fe616d37c751eacea1c12d93f1
koknaev <89024464050@yandex.ru>
2021-04-04 16:28:08 +0500

    8

--- app/src/main/java/com/yandex/koknaev/criminalintent/MainActivity.kt
+++ app/src/main/java/com/yandex/koknaev/criminalintent/MainActivity.kt
@@ -2,10 +2,20 @@ package com.yandex.koknaev.criminalintent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yandex.koknaev.criminalintent.ui.CrimeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+        if (currentFragment == null) {
+            val fragment = CrimeFragment()
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit()
+        }
     }
 }
